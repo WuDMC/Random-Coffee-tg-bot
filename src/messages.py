6d26539c67423e270be4e73175973742232c7c3e
Denis <wudmccpa@gmail.com>
2022-03-23 20:48:09 +0300

    отладка степ2

--- src/messages.py
+++ src/messages.py
@@ -2,15 +2,17 @@ import re
 import string
 import secrets
 
-from settings import COMPANY
-
 re_mail = re.compile(r'\b.*\b')
 alphabet = string.ascii_letters + string.digits
 
 
 def is_correct_mail(mail):
-    return re_mail.fullmatch(mail) and mail.endswith(f'@{COMPANY}')
+    return re_mail.fullmatch(mail)
 
 
 def generate_password():
-    return ''.join(secrets.choice(alphabet) for i in range(20))
+    passwrd = 'Batumi'
+    print(passwrd)
+    return passwrd
+
+generate_password()
