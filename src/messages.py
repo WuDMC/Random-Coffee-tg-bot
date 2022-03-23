01d1ece0097c38d79e4a27e079f041a0075cbf5f
Denis <wudmccpa@gmail.com>
2022-03-23 20:10:21 +0300

    отладка степ2

--- src/messages.py
+++ src/messages.py
@@ -4,12 +4,12 @@ import secrets
 
 from settings import COMPANY
 
-re_mail = re.compile(r'\b.*\b')
+re_mail = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
 alphabet = string.ascii_letters + string.digits
 
 
 def is_correct_mail(mail):
-    return re_mail.fullmatch(mail)
+    return re_mail.fullmatch(mail) and mail.endswith(f'@{COMPANY}')
 
 
 def generate_password():
