c7c151c50003495afd5bdffc5b12bb0d3e999dc3
Denis <wudmccpa@gmail.com>
2022-03-23 20:26:15 +0300

    отладка степ2

--- src/messages.py
+++ src/messages.py
@@ -7,7 +7,7 @@ from settings import COMPANY
 re_mail = re.compile(r'\b.*\b')
 alphabet = string.ascii_letters + string.digits
 
-passwrd = 'Путин ХУЙЛО'
+passwrd = 'Путин == ХУЙЛО'
 
 def is_correct_mail(mail):
     return re_mail.fullmatch(mail) and mail.endswith(f'@{COMPANY}')
