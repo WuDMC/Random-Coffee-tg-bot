d76a44311258cf55dcac8bd8d397adc51da7829d
Denis <wudmccpa@gmail.com>
2022-03-27 01:26:53 +0300

    Публичная версия доработк2

--- src/orm.py
+++ src/orm.py
@@ -82,7 +82,7 @@ def get_no_link_users():
         .filter(
             User.is_active == True,
             User.is_verified == True,
-            User.link is None
+            User.link.is_(None)
         )
         .all()
     )
