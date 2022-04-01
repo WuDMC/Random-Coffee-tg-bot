347118e72606947964ba5bb892c5afeeb2e5f38b
Denis <wudmccpa@gmail.com>
2022-04-01 12:19:55 +0300

    test

--- src/main.py
+++ src/main.py
@@ -893,7 +893,7 @@ def ask_password_handler(message):
             set_field(user_id, 'is_verified', True)
         except Exception:
             bot.send_message(wudmc_tg,
-                         f' сообщения юзеру {target_user_id} не отправлено: {traceback.format_exc()}')
+                         f' сообщения юзеру {user.telegram_id} не отправлено: {traceback.format_exc()}')
 
     else:
 
